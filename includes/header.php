8c6cbf303e848a2564cadbc7eceae38111a8ef8b
VISHNU R <105124943+VishnuR123@users.noreply.github.com>
2023-09-25 21:31:31 +0530

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,51 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<div class="navbar navbar-inverse navbar-fixed-top">    
+<div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>                        
+            </button>
+            <a class="navbar-brand" href="index.php">Webmart | E-commerce site</a>
+        </div>
+        
+        
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+
+                 
+
+                <?php
+                if (isset($_SESSION['email'])) {
+                    ?>
+                    <li><a href = "cart.php"><span class = "glyphicon glyphicon-shopping-cart"></span> Cart </a></li>
+                    <li><a href = "settings.php"><span class = "glyphicon glyphicon-user"></span> Settings</a></li>
+                    <li><a href = "logout_script.php"><span class = "glyphicon glyphicon-log-in"></span> Logout</a></li>
+                    ?>
+                    <?php
+                } else {
+                    ?>
+                    <li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                        <?php
+                    }
+                    ?>
+
+            </ul>
+
+
+<div class="form-group input-group">
+
+
+</div>
+
+</form>
+
+             
+        </div>
+        </div>
+    </div>
+</div>
